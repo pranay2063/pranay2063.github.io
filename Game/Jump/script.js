22917ca529d598439bcee291d479fb8007495c74
Pranay Ranjan <pranay2063@users.noreply.github.com>
2017-04-02 05:43:48 +0530

    Create script.js

---
+++ Game/Jump/script.js
@@ -0,0 +1,55 @@
+
+var canvas = document.querySelector('#myCanvas');
+context = canvas.getContext('2d');
+
+/*
+context.beginPath();
+context.rect(20,40,50,50);
+context.fillStyle = "red";
+context.fill();
+//context.strokeStyle = "red";
+//context.stroke();
+context.closePath();
+
+context.beginPath();
+context.arc(240, 160, 20, 0, Math.PI*2, false);
+context.fillStyle = "green";
+context.fill();
+context.closePath();
+*/
+
+var x = canvas.width/2;
+var y = canvas.height-30;
+
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
+
+function drawBall(){
+	context.beginPath();
+	context.arc(x, y, ballRadius, 0, Math.PI*2, false);
+	context.fillStyle = "blue";
+	context.fill();
+	context.closePath();
+}
+
+function draw(){
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+	x += dx;
+	y += dy;
+
+	if(x < 0) dx = -dx;
+	if(y < 0) dy = -dy;
+	if(x > canvas.width)	dx = -dx;
+	if(y > canvas.height)	dy = -dy;
+}
+
+setInterval(draw, 1);
+
+
+
+
+
+
+

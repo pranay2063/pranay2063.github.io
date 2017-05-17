fde0466c3e6d51491ff137511bb7b21ff7545070
Pranay Ranjan <pranay2063@users.noreply.github.com>
2017-05-18 02:07:34 +0530

    Update main.js

--- Concepts/main.js
+++ Concepts/main.js
@@ -2,6 +2,6 @@
 $('ol li').wrapInner('<span class="diff" ></span>');
 $('.diff').css('color', '#008080');
 $('a').attr('target', '_self');
-$('ol li').css('text-align', 'center');
+$('ol').css({'text-align' : 'center', 'list-style-position' : 'inside'});
 //$('ol').replaceWith($('<ul>'+ $('ol').html() +'</ul>'));
-
+$
